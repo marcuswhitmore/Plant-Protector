bce8a1a0ad7bcfd2d542140ed4219fc405b10da8
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-07-27 11:31:09 -0500

    update axios

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { FlatList, Text, StyleSheet, Image, View } from 'react-native';
 
 const rows = [
-  { id: 0, text: 'Pepper', posters: 'https://i.imgur.com/UePbdph.jpg' },
-  { id: 1, text: 'Pepper' },
-  { id: 2, text: 'Pepper' },
-  { id: 3, text: 'Pepper' },
-  { id: 4, text: 'Pepper2' },
+  { id: 0, cultivar: 'Carolina Reaper', scoville: ' 10000 SHU' },
+  { id: 1, cultivar: 'Jalapeno', scoville: ' 10000 SHU' },
+  { id: 2, cultivar: 'Poblano', scoville: ' 10000 SHU' },
+  { id: 3, cultivar: 'Habanero', scoville: ' 10000 SHU' },
+  { id: 4, cultivar: 'Ghost Pepper', scoville: ' 10000 SHU' },
 ]
 
 const extractKey = ({ id }) => id
@@ -17,8 +17,8 @@ export default class App extends Component {
     return (
   
       <Text style={styles.row}>
-        {item.text}
-        {item.posters}
+        {item.cultivar}
+        {item.scoville}
         
       </Text>
       
