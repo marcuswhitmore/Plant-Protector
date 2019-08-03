0db867ec139394d81b6207c76c47f79001ef0b17
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-08-03 10:20:36 -0500

    add weather

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,33 +1,23 @@
 import React, { Component } from 'react';
 import { AppRegistry, View, Text, StyleSheet, Alert, TouchableHighlight } from 'react-native';
-import ReactWeather from 'react-open-weather';
+import { WeatherWidget } from 'react-native-weather';
 
 export default class FlexDimensionsBasics extends Component {
   render() {
     return (
-      // Try removing the `flex: 1` on the parent View.
-      // The parent will not have dimensions, so the children can't expand.
-      // What if you add `height: 300` instead of `flex: 1`?
       <View style={{flex: 1, backgroundColor: '#3F4447'}}>
         <View style={{flex: 1, backgroundColor: 'powderblue'}} >
         <Text style={styles.text}> Plant Protector </Text>
         </View>
         <View style={{flex: 3, backgroundColor: 'skyblue'}} >
-        <TouchableHighlight style={styles.wrapper}
-          onPress={() => Alert.alert(
-            null,
-            null,
-            [
-              {text: 'Button', onPress: () => console.log('Button Pressed!')},
-              // on press send request via https to Pid
-            ]
-          )}>
-          <View style={styles.button}>
-            <Text>Alert with only one button</Text>
-          </View>
-        </TouchableHighlight>
+        <Text style={styles.text}>Weather go info goes here</Text>
+       
         </View>
-        
+        <WeatherWidget
+          api={"99b722aa5365f77af4c004c0cca23ef9"}
+          lat={40.3601}
+          long={-71.0589}
+        />
         
  
       </View>
