a1239371813aac7ae022895429d26ac0636f2c8e
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-08-13 20:31:55 -0500

    login

--- App.js
+++ App.js
@@ -1,32 +1,50 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
+import {
+  AppLoading
+} from 'expo';
+import {
+  Asset
+} from 'expo-asset';
 import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, ViewStyleSheet, Text,View,TextInput, Button,TouchableHighlight,
+import React, {
+  useState
+} from 'react';
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  ViewStyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
   Image,
-  Alert } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-//import firebase from "./utils/firebase";
+  Alert
+} from 'react-native';
+import {
+  Ionicons
+} from '@expo/vector-icons';
+import firebase from "./utils/firebase";
 
 
- ////var firebaseConfig = {
- ////  apiKey: "AIzaSyC3FmjDz7y6_cQE4zWrwguoCqCe7HxgT9M",
- ////  authDomain: "plant-protector-27ea5.firebaseapp.com",
- ////  databaseURL: "https://plant-protector-27ea5.firebaseio.com",
- ////  projectId: "plant-protector-27ea5",
- ////  storageBucket: "",
- ////  messagingSenderId: "6658779484",
- ////  appId: "1:6658779484:web:d7b4e231f598a527"
- ////};
+////var firebaseConfig = {
+////  apiKey: "AIzaSyC3FmjDz7y6_cQE4zWrwguoCqCe7HxgT9M",
+////  authDomain: "plant-protector-27ea5.firebaseapp.com",
+////  databaseURL: "https://plant-protector-27ea5.firebaseio.com",
+////  projectId: "plant-protector-27ea5",
+////  storageBucket: "",
+////  messagingSenderId: "6658779484",
+////  appId: "1:6658779484:web:d7b4e231f598a527"
+////};
 //Initialize Firebase
 
 import AppNavigator from './navigation/AppNavigator';
 
 //firebase.initializeApp(firebaseConfig);
 export default function App(props) {
-  
-  
- 
+
+
+
 
   const [isLoadingComplete, setLoadingComplete] = useState(false);
   // const [username, setUserName] = useState("");
@@ -34,19 +52,19 @@ export default function App(props) {
 
 
 
- 
 
-  
+
+
   // registerUser = event => {
-   
+
   //     firebase.auth().createUserWithEmailAndPassword(username, password).catch(function(error) {
   //       // Handle Errors here.
   //       var errorCode = error.code;
   //       var errorMessage = error.message;
   //     })};
-      
+
   //     signInUser = event => {
-        
+
   //       firebase
   //         .auth()
   //         .signInWithEmailAndPassword(username, password)
@@ -55,28 +73,35 @@ export default function App(props) {
   //           var errorCode = error.code;
   //           var errorMessage = error.message;
   //         })};
-            
-          
+
+
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return (
-      
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
+
+      <
+      AppLoading startAsync = {
+        loadResourcesAsync
+      }
+      onError = {
+        handleLoadingError
+      }
+      onFinish = {
+        () => handleFinishLoading(setLoadingComplete)
+      }
       />
     );
-  }else{
-  //  console.log(user.email);
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
+  } else {
+    //  console.log(user.email);
+    return ( 
+      < View style = {styles.container } >
+         {Platform.OS === 'ios' && < StatusBar barStyle = "default" />
+      } 
+      <AppNavigator />
       </View>
     );
-    
-  } 
+
+  }
 };
 async function loadResourcesAsync() {
   await Promise.all([
@@ -108,52 +133,52 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    },
-    loadingContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#FFFDE4'
-    },
-    loadingText: {
-        fontSize: 30
-    },
-    inputContainer: {
-      borderBottomColor: "#F5FCFF",
-      backgroundColor: "#FFFFFF",
-      borderRadius: 30,
-      borderBottomWidth: 1,
-      width: 250,
-      height: 45,
-      marginBottom: 20,
-      flexDirection: "row",
-      alignItems: "center"
-    },
-    inputs: {
-      height: 45,
-      marginLeft: 16,
-      borderBottomColor: "#FFFFFF",
-      flex: 1
-    },
-    inputIcon: {
-      width: 30,
-      height: 30,
-      marginLeft: 15,
-      justifyContent: "center"
-    },
-    buttonContainer: {
-      height: 45,
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-      marginBottom: 20,
-      width: 250,
-      borderRadius: 30
-    },
-    loginButton: {
-      backgroundColor: "#00b5ec"
-    },
-    loginText: {
-      color: "white"
-    }
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFDE4'
+  },
+  loadingText: {
+    fontSize: 30
+  },
+  inputContainer: {
+    borderBottomColor: "#F5FCFF",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 250,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: "#FFFFFF",
+    flex: 1
+  },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 15,
+    justifyContent: "center"
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30
+  },
+  loginButton: {
+    backgroundColor: "#00b5ec"
+  },
+  loginText: {
+    color: "white"
+  }
 });
